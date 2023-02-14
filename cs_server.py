6660ac72265b8c07a412f8e625f9d71586a54642
Robby Roda <95758113+CoderSigma@users.noreply.github.com>
2023-02-15 01:58:45 +0800

    Update cs_server.py

--- cs_server.py
+++ cs_server.py
@@ -53,4 +53,59 @@ def receive():
         thread.start()
         
 print("Nakikinis sayo ang server")
+receive()import threading
+import socket
+
+host = '127.0.0.1'
+#local host to ahhh
+
+port = 44444
+#eto port mo syempre
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+
+
+client = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+        
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            #yung 1024 bytes po sya okie??
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nicknames = nickname[index]
+            broadcast(f'{nickname} left the chat' .encode('ascii'))
+            nicknames.remove(nickname)
+            break
+ 
+ 
+def receive():
+    while True:
+        client, address = server.accept()
+        print("connected with {str(address)}") 
+        
+        client.send('NICK' .encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append()
+        
+        print(f'Nickname of the client is {nickname}!')
+        broadcast(f'{nickname} joined the server!' .ecode('ascii'))
+        
+        thread = threading.Thread(target= handle, args=(client,))
+        thread.start()
+        
+print("Nakikinis sayo ang server")
 receive()
