012af1733ad6a37eecba1b3615e03210f6684b3f
Robby Roda <95758113+CoderSigma@users.noreply.github.com>
2023-02-15 02:23:04 +0800

    Update cs_Client.py

--- cs_Client.py
+++ cs_Client.py
@@ -4,7 +4,36 @@ import socket
 nickname = input("Enter your nickname: ")
 
 client = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 4444))
+client.connect(('127.0.0.1', 5555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+             print("Error occured syempre ano aasahan mo?")
+             client.close()
+             break
+             
+def write():
+    while True:
+        message = f'{nickname}: {input(" ")}'
+        client.send(message.encode('ascii'))
+receive_threads = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()import threading
+import socket
+
+nickname = input("Enter your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 44444))
 
 def receive():
     while True:
